4debb588302e3c8a34b5ad34ca8f61b73df37a3a
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-04-26 07:45:57 -0700

    simple_shell

--- shell.h
+++ shell.h
@@ -23,36 +23,15 @@ void print_enviro(char **env_point);
 void liberay_p(char **arr, int s);
 void _path(char **arr, int s, char **_enviro, int *wstatus);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char **_strtok(char *line, char *delim);
-char *get_location(char *command);
-list_t *get_path_dir(char *path);
 int execute(char **args, char **front);
-void free_list(list_t *head);
-char *_itoa(int num);
-void handle_line(char **line, ssize_t read);
-void variable_replacement(char **args, int *exe_ret);
-char *get_args(char *line, int *exe_ret);
-int call_args(char **args, char **front, int *exe_ret);
-int run_args(char **args, char **front, int *exe_ret);
-int handle_args(int *exe_ret);
-int check_args(char **args);
-void free_args(char **args, char **front);
-char **replace_aliases(char **args);
-
 /*   Generic functions. */
 
 char *str_cpy(char *after, char *src, int n);
 int str_len(char *str);
 int str_cmp(char *s1, char *s2);
 char *str_cat(char *after, char *src);
-int _strlen(const char *s);
-char *_strcat(char *dest, const char *src);
-char *_strncat(char *dest, const char *src, size_t n);
-char *_strcpy(char *dest, const char *src);
 char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
-int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 
 #include <string.h>
@@ -64,8 +43,6 @@ int _strncmp(const char *s1, const char *s2, size_t n);
 #include <errno.h>
 #include <time.h>
 
-#include "ansi.h"
-#include "_getline.h"
 
 /* for read/write buffers */
 #define READ_BUF_SIZE 1024
@@ -87,186 +64,4 @@ int _strncmp(const char *s1, const char *s2, size_t n);
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 
-#define HIST_FILE	".simple_shell_history"
-#define HIST_MAX	4096
-
-extern char **environ;
-
-/**
- * struct liststr - singly linked list
- * @num: the number field
- * @str: a string
- * @next: points to the next node
- */
-typedef struct liststr
-{
-	int num;
-	char *str;
-	struct liststr *next;
-} list_t;
-
-/**
- * struct list_s - A new struct type defining a linked list.
- * @dir: A directory path.
- * @next: A pointer to another struct list_s.
- */
-
-typedef struct list_s
-{
-	char *dir;
-	struct list_s *next;
-} list_t;
-
-/**
- * struct builtin_s - A new struct type defining builtin commands.
- * @name: The name of the builtin command.
- * @f: A function pointer to the builtin command's function.
- */
-
-typedef struct builtin_s
-{
-	char *name;
-	int (*f)(char **argv, char **front);
-} builtin_t;
-
-/**
- * struct alias_s - A new struct defining aliases.
- * @name: The name of the alias.
- * @value: The value of the alias.
- * @next: A pointer to another struct alias_s.
- */
-
-typedef struct alias_s
-{
-	char *name;
-	char *value;
-	struct alias_s *next;
-} alias_t;
-
-/* Global aliases linked list */
-alias_t *aliases;
-
-/* Builtins */
-int (*get_builtin(char *command))(char **args, char **front);
-int shell_exit(char **args, char **front);
-int shell_env(char **args, char __attribute__((__unused__)) **front);
-int shell_setenv(char **args, char __attribute__((__unused__)) **front);
-int shell_unsetenv(char **args, char __attribute__((__unused__)) **front);
-int shell_cd(char **args, char __attribute__((__unused__)) **front);
-int shell_alias(char **args, char __attribute__((__unused__)) **front);
-int shell_help(char **args, char __attribute__((__unused__)) **front);
-
-/* Builtin */
-char **_copyenv(void);
-void free_env(void);
-char **_getenv(const char *var);
-
-/* Error */
-int create_error(char **args, int err);
-char *error_env(char **args);
-char *error_1(char **args);
-char *error_2_exit(char **args);
-char *error_2_cd(char **args);
-char *error_2_syntax(char **args);
-char *error_126(char **args);
-char *error_127(char **args);
-
-/* list Helpers */
-alias_t *add_alias_end(alias_t **head, char *name, char *value);
-void free_alias_list(alias_t *head);
-list_t *add_node_end(list_t **head, char *dir);
-void free_list(list_t *head);
-
-void help_all(void);
-void help_alias(void);
-void help_cd(void);
-void help_exit(void);
-void help_help(void);
-void help_env(void);
-void help_setenv(void);
-void help_unsetenv(void);
-void help_history(void);
-
-int proc_file_commands(char *file_path, int *exe_ret);
-
-/**
- *struct passinfo - contains pseudo-arguements to pass into a function,
- *					allowing uniform prototype for function pointer struct
- * @arg: a string generated from getline containing arguements
- * @argv: an array of strings generated from arg
- * @path: a string path for the current command
- * @argc: the argument count
- * @line_count: the error count
- * @err_num: the error code for exit()s
- * @linecount_flag: if on count this line of input
- * @fname: the program filename
- * @env: linked list local copy of environ
- * @environ: custom modified copy of environ from LL env
- * @history: the history node
- * @alias: the alias node
- * @env_changed: on if environ was changed
- * @status: the return status of the last exec'd command
- * @cmd_buf: address of pointer to cmd_buf, on if chaining
- * @cmd_buf_type: CMD_type ||, &&, ;
- * @readfd: the fd from which to read line input
- * @histcount: the history line number count
- * @left_redirect_from_fd: the fd to left redirect from
- * @left_append: true if heredoc
- * @right_redirect_from_fd: fd right redirecting from (default 1)
- * @right_redirect_to_fd: fd right redirecting to
- * @right_append: true if right stream appends
- * @heredoc: value of HEREDOC delimeter
- * @heredoc_txt: accumulated HEREDOC lines
- * @heredoc_cmd: the command to pipe HEREDOC line
- * @help: help flags
- * @pipefd: fd pipe for interprocess communication | pipe
- * @startup_fd: fd of startup file or -1
- * @dup_stdin: saved duplicate of stdin for restoration after redirect
- * @dup_stdout: saved duplicate of stdout for restoration after redirect
- */
-typedef struct passinfo
-{
-	char *arg;
-	char **argv;
-	char *path;
-	int argc;
-	unsigned int line_count;
-	int err_num;
-	int linecount_flag;
-	char *fname;
-	list_t *env;
-	list_t *history;
-	list_t *alias;
-	char **environ;
-	int env_changed;
-	int status;
-
-	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
-	int cmd_buf_type; /* CMD_type ||, &&, ; */
-	int readfd;
-	int histcount;
-
-	int left_redirect_from_fd;
-	int left_append;
-
-	int right_redirect_from_fd;
-	int right_redirect_to_fd;
-	int right_append;
-
-	char *heredoc;
-	char *heredoc_txt;
-	char *heredoc_cmd;
-	char *help;
-
-	int pipefd[2];
-	int startup_fd;
-	int dup_stdin;
-	int dup_stdout;
-
-} info_t;
-
-#define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-0, 0, 0}
-
-#endif /* _HEADERS_H_ */
+#endif
