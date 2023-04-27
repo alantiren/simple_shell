074e01966e472d5bfb175f855b4b7bacc2198291
alantiren <tirenalan0@gmail.com>
2023-04-27 17:16:35 +0300

    update

--- shell.h
+++ shell.h
@@ -29,7 +29,6 @@ void _path(char **arr, int s, char **_enviro, int *wstatus);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 int execute(char **args, char **front);
 int shell_loop(void);
-void execute_command(char* command)
 
 /*   Generic functions. */
 
