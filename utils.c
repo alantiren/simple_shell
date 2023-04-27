a531520e136155bb322b96d061a972e43534ef9a
alantiren <tirenalan0@gmail.com>
2023-04-27 16:39:31 +0300

    update

--- utils.c
+++ utils.c
@@ -2,17 +2,20 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-/* is_valid_executable - function prototype with the correct parameter and return types
+/* is_valid_executable - function prototype
 * @access - system call to check
 * Function for checking if a string represents a valid executable file
 * Returns : true. Otherwise, it returns false
-**/ 
+**/
 
 bool is_valid_executable(const char *filename)
 {
-    if (access(filename, X_OK) == 0) {
-        return (true);
-    } else {
-        return (false);
-    }
+if (access(filename, X_OK) == 0)
+{
+return (true);
+}
+else
+{
+return (false);
+}
 }
